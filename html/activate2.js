b011aceb1f1aab6a1a7e2767856fa5f2e37d7e67
Scott Bailey <scott.j.bailey@hotmail.com>
2026-05-15 23:17:21 -0700

    Initial commit

---
+++ html/activate2.js
@@ -0,0 +1,7 @@
+document.write('<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,0,0" width="846" height="325" id="bassetti" align="middle">');
+document.write('<param name="allowScriptAccess" value="sameDomain" />');
+document.write('<param name="movie" value="vs.swf" />');
+document.write('<param name="quality" value="high" />');
+document.write('<param name="bgcolor" value="#ffffff" />');
+document.write('<embed src="vs.swf" quality="high" bgcolor="#ffffff" width="846" height="325" name="bassetti" align="middle" allowScriptAccess="sameDomain" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />');
+document.write('</object>');
